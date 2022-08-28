32e9048a4670e0d9e192bd92f9874e6844754bf1
Vitalii Travin <vitalii.trvn@gmail.com>
2022-08-28 18:51:55 +0300

    react seat booking app

--- src/components/Script.jsx
+++ src/components/Script.jsx
@@ -10,16 +10,18 @@ const options = [
   { value: '20:00', label: '20:00' },
 ];
 
+const listSeats = [
+  { id: 0, name: '01', busy: false },
+  { id: 1, name: '02', busy: false },
+  { id: 2, name: '03', busy: false },
+  { id: 3, name: '04', busy: false },
+  { id: 4, name: '05', busy: false },
+  { id: 5, name: '06', busy: false },
+];
+
 const Script = () => {
   const [selectedOption, setSelectedOption] = useState(null);
-  const [seats, setSeats] = useState([
-    { id: 0, name: '01', busy: false },
-    { id: 1, name: '02', busy: false },
-    { id: 2, name: '03', busy: false },
-    { id: 3, name: '04', busy: false },
-    { id: 4, name: '05', busy: false },
-    { id: 5, name: '06', busy: false },
-  ]);
+  const [seats, setSeats] = useState(listSeats);
 
   const onClickPlace = (id) => {
     const selectedSeat = seats.map((seat) => {
@@ -30,11 +32,27 @@ const Script = () => {
 
   const onClickBtn = () => {
     const activeSeats = seats.filter((seat) => seat.busy === true);
-    console.log(activeSeats);
     const seatsIndex = [...activeSeats].map((seat) => [...seats].indexOf(seat));
-    localStorage.setItem('selectedSeats', JSON.stringify(activeSeats));
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
     alert(`Вы приобрели ${activeSeats.length} билета`);
   };
+
+  // function seatsFromLS() {
+  //   const selectedSeatsIndx = JSON.parse(localStorage.getItem('selectedSeats'));
+  //   console.log('selectedSeatsIndx', selectedSeatsIndx);
+  //   if (selectedSeatsIndx !== null && selectedSeatsIndx.length > 0) {
+  //     let arr = [];
+  //     arr = seats.forEach((seat, index) => {
+  //       console.log('seat-indx', index);
+  //       return selectedSeatsIndx.indexOf(index) > -1 ? { ...seat, busy: true } : { ...seat };
+  //       setSeats(arr);
+  //     });
+  //   }
+  // }
+  // console.log(seats);
+  // React.useEffect(() => {
+  //   seatsFromLS();
+  // }, []);
 
   const className = (seat) => {
     return 'seat' + (seat.busy ? '-seat-busy' : '');
